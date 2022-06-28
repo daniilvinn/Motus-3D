908b82ca70b836ef1df2cef4c258faf3da027216
daniilvinn <86977774+daniilvinn@users.noreply.github.com>
2022-06-28 23:01:23 +0300

    Added test layer in Sandbox project

--- Sandbox2D/src/Sandbox2D.cpp
+++ Sandbox2D/src/Sandbox2D.cpp
@@ -1,19 +1,43 @@
 #include <Motus3D.h>
 
+class TestLayer : public Motus3D::Layer
+{
+public:
+	void OnUpdate() override
+	{
+		
+	};
+
+	void OnEvent(Event& e) override
+	{
+		
+	};
+
+	void OnAttach() override
+	{
+		MT_LOG_TRACE("Test layer attached");
+	};
+
+	void OnDetach() override
+	{
+		MT_LOG_TRACE("Test layer detached");
+	};
+
+private:
+
+};
 class Sandbox2D : public Motus3D::Application
 {
 public:
-	Sandbox2D(){};
+	Sandbox2D()
+	{
+		AddLayer(new TestLayer());
+	};
 	~Sandbox2D() override
 	{
 	};
 };
 
-class TestLayer : public Motus3D::Layer
-{
-public:
-
-};
 
 Motus3D::Scope<Motus3D::Application> CreateApplication()
 {
