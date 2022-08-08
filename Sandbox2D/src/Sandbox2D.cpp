28dc229b7a88a31ac3673bd8f8df394af6f624a5
daniilvinn <86977774+daniilvinn@users.noreply.github.com>
2022-08-08 16:22:05 +0300

    Updated Sandbox2D
    Updated Sandbox2D, which now utilizes new Motus3D's features, such as dynamic shader data (push-constants). Also implemented basic intertia simulation.

--- Sandbox2D/src/Sandbox2D.cpp
+++ Sandbox2D/src/Sandbox2D.cpp
@@ -1,4 +1,9 @@
 #include <Motus3D.h>
+
+#define GLM_FORCE_SSE2
+#define GLM_FORCE_ALIGNED
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 using namespace Motus3D;
 
 class TestLayer : public Motus3D::Layer
@@ -6,15 +11,40 @@ class TestLayer : public Motus3D::Layer
 public:
 	void OnUpdate() override
 	{
-		Renderer::BeginRender();
-		Renderer::Submit(m_VBO, m_IBO, m_Pipeline);
-		Renderer::EndRender();
+		if (Input::KeyPressed(KeyCode::KEY_W)) {
+			//m_QuadPosition.y += 0.001f;
+			m_Acceleration.y = 0.0f;
+			m_Acceleration.y += 0.001f;
+			m_AccelerationMagnitude.y = 1.0f;
+		};
+		if (Input::KeyPressed(KeyCode::KEY_A)) {
+			//m_QuadPosition.x -= 0.001f;
+			m_Acceleration.x = 0.0f;
+			m_Acceleration.x -= 0.001f;
+			m_AccelerationMagnitude.x = 1.0f;
+		};
+		if (Input::KeyPressed(KeyCode::KEY_S)) {
+			//m_QuadPosition.y -= 0.001f;
+			m_Acceleration.y = 0.0f;
+			m_Acceleration.y -= 0.001f;
+			m_AccelerationMagnitude.y = 1.0f;
+		};
+		if (Input::KeyPressed(KeyCode::KEY_D)) {
+			//m_QuadPosition.x += 0.001f;
+			m_Acceleration.x = 0.0f;
+			m_Acceleration.x += 0.001f;
+			m_AccelerationMagnitude.x = 1.0f;
+		};
 
-	};
+		m_QuadPosition.x += m_Acceleration.x * m_AccelerationMagnitude.x;
+		m_QuadPosition.y += m_Acceleration.y * m_AccelerationMagnitude.y;
+		m_AccelerationMagnitude -= 0.001f;
+		if (m_AccelerationMagnitude.x < 0.0f) m_AccelerationMagnitude.x = 0.0f;
+		if (m_AccelerationMagnitude.y < 0.0f) m_AccelerationMagnitude.y = 0.0f;
 
-	void OnEvent(Event& e) override
-	{
-		
+		Renderer::BeginScene({ m_VPmatrix });
+		Renderer::Submit(m_VBO, m_IBO, m_Pipeline, m_QuadPosition);
+		Renderer::EndScene();
 	};
 
 	void OnAttach() override
@@ -45,6 +75,13 @@ public:
 
 		m_VBO = VertexBuffer::Create(vertices, sizeof(vertices), 0);
 		m_IBO = IndexBuffer::Create(indices, sizeof(indices), 0, IndexType::UINT8);
+		
+		m_VPmatrix = glm::perspective(45.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+
+		// Physics
+		m_QuadPosition = glm::vec3(0.0f, 0.0f, -10.0f);
+		m_Acceleration = glm::vec3(0.0f);
+		m_AccelerationMagnitude = glm::vec3(0.0f);
 
 		MT_LOG_TRACE("Test layer attached");
 	};
@@ -57,12 +94,35 @@ public:
 		MT_LOG_TRACE("Test layer detached");
 	};
 
+	void OnEvent(Event& e) override 
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowResizedEvent>(MT_BIND_EVENT_FUNCTION(TestLayer::OnWindowResize));
+	};
+
 private:
 
+	bool OnWindowResize(WindowResizedEvent& e) 
+	{
+		m_VPmatrix = glm::perspective(45.0f, (float)e.width / (float)e.height, 0.1f, 100.0f);
+		return false;
+	}
+
+private:
+
+	// Static data
 	Ref<VertexBuffer> m_VBO;
 	Ref<IndexBuffer> m_IBO;
 	Ref<Pipeline> m_Pipeline;
 	Ref<Shader> m_Shader;
+
+	// Dynamic data
+	glm::mat4 m_VPmatrix;
+
+	// Physics
+	glm::vec3 m_QuadPosition;
+	glm::vec3 m_Acceleration;
+	glm::vec3 m_AccelerationMagnitude;
 
 };
 class Sandbox2D : public Motus3D::Application
